ba5af7f8965f35832800210db7b44be63bc268b9
makeboluo000 <makeboluo000@163.com>
2019-05-15 23:15:50 +0800

    feign 结合hystrix提交

--- client/src/main/java/com/imooc/product/client/ProductClient.java
+++ client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -3,12 +3,13 @@ package com.imooc.product.client;
 import com.imooc.product.common.DecreaseStockInput;
 import com.imooc.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name="product")
+@FeignClient(name="product", fallback = ProductClient.ProductClientFallBack.class)
 public interface ProductClient {
 
     /**
@@ -21,5 +22,19 @@ public interface ProductClient {
 
     @PostMapping("/product/decreaseStock")
     public void decreaseStock(@RequestBody List<DecreaseStockInput> decreaseStockInputList);
+
+    @Component // 一定要加 加入到spring容器管理
+    class ProductClientFallBack implements ProductClient {
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+    }
 
 }
